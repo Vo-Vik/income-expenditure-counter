28592f57ed8adf169893cf4948bea3f6f95a4df7
Vo_Vik <vovik@vovik-laptop.(none)>
2012-10-24 23:09:08 -0400

    Added accessControl for index page

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,6 +20,40 @@ class SiteController extends Controller
 			),
 		);
 	}
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  //allow all users to perform 'contact' and 'login' actions
+				'actions'=>array('contact','login','page'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('index','logout'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array(),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
 
 	/**
 	 * This is the default 'index' action that is invoked
